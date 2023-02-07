97b229f3c8b1b40fa00ca55875999821f52d63ba
qoalstjd <bms980805@gmail.com>
2023-02-07 17:56:16 +0900

    프로젝트, 메인 > KB, 신한, mshop 수정

--- src/js/animate.js
+++ src/js/animate.js
@@ -1,46 +1,50 @@
-
-
 let $body = document.querySelector("body");
-let animatedTexts = $body.querySelectorAll('.animated-text');
+let animatedTextsV1 = $body.querySelectorAll(".animated-text.v1");
+let animatedTextsV2 = $body.querySelector(".animated-text.v2");
 
-//console.log(animatedTexts)
-        
-function animateText(){
-    // Text
-    animatedTexts.forEach(function(el) { 
-      //let tl = new TimelineLite;
-      let splitText = el.innerHTML.split("<br>");
-      let temp = []; 
-      //console.log(el)
+//console.log(animatedTextsV1)
 
-      splitText.forEach(function(text,idx){
-        let txt = text.split(" ");
-        let arr = [];
-        txt.forEach(function(t){
-          arr.push(t.replace(/([^\x00-\x80]|\w|.)/g, "<span class='_text'>$&</span>"))
-        })
-        //temp.push(arr.map(t => `<div class='_box'>${t}</div>`).join(""))
-        temp.push(
-          arr.map(function(t){
-            return `<div class='_box'>${t}</div>`
-          }).join("")
-        )
-        if(idx != splitText.length -1) {
-          temp.push("<br>")
-        }
-      })
-      el.innerHTML = temp.join("")
-      //console.log(el.innerHTML)
+function animateTextV1() {
+  // Text
+  animatedTextsV1.forEach(function (el) {
+    //let tl = new TimelineLite;
+    let splitText = el.innerHTML.split("<br>");
+    let temp = [];
+    //console.log(el)
 
+    splitText.forEach(function (text, idx) {
+      let txt = text.split(" ");
+      let arr = [];
+      txt.forEach(function (t) {
+        arr.push(t.replace(/([^\x00-\x80]|\w|.)/g, "<span class='_text'>$&</span>"));
+      });
+      //temp.push(arr.map(t => `<div class='_box'>${t}</div>`).join(""))
+      temp.push(
+        arr
+          .map(function (t) {
+            return `<div class='_box'>${t}</div>`;
+          })
+          .join(""),
+      );
+      if (idx != splitText.length - 1) {
+        temp.push("<br>");
+      }
     });
+    el.innerHTML = temp.join("");
+    //console.log(el.innerHTML)
+  });
 
-    let $text = document.querySelectorAll('.animated-text span');
-    let tl = new TimelineMax();
+  let $text = document.querySelectorAll(".animated-text span");
+  let tl = new TimelineMax();
 
-    tl
-      .delay(2)
-      .staggerFrom($text, 0.2, {alpha: 0,y:50, z: 0, ease:Linear.easeNone}, 0.05)
+  tl.delay(2).staggerFrom($text, 0.2, { alpha: 0, y: 50, z: 0, ease: Linear.easeNone }, 0.05);
 }
 
+animateTextV1();
 
-animateText();
+function animateTextV2() {
+  animatedTextsV2.style.opacity = 1;
+  animatedTextsV2.style.transform = "translateY(0px)";
+}
+
+animateTextV2();
